85035b0dfd6cdd3662f17e330935f1092d0a1dc5
takumi2003 <takumi20030819@gmail.com>
2024-04-10 03:39:37 +0900

    create express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import express, { Application, Request, Response } from 'express';
+import { load } from 'ts-dotenv';
+const env = load({
+  PORT: Number,
+});
+
+const PORT = env.PORT || 3000;
+
+const app: Application = express();
+
+app.get('/', async (_: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}/`);
+});
